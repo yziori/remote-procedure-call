c7587fff6c0d6831f465485475d6e27a1d9bad36
yziori <ms6rx7861@gmail.com>
2024-06-02 20:16:03 +0900

    🚧 work: リクエストとレスポンスのモデル実装

---
+++ client/src/models.ts
@@ -0,0 +1,13 @@
+export interface Request {
+    method: string;
+    params: any[];
+    param_type: string[];
+    id: number
+}
+
+export interface Response {
+    result: any;
+    result_type: string;
+    id: number
+    error: string;
+}
